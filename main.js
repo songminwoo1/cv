1dab2a9912c8bca394c21a2e0cc44b9cbb5b3dda
songminwoo1 <57557231+songminwoo1@users.noreply.github.com>
2023-11-17 13:29:05 +0900

    init

---
+++ main.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+    Init();
+    console.log(mWins);
+};
+
+var mWins = [];
+var mWinMov = [];
+
+const Init = () =>
+{
+    mWins.push(document.getElementById("profile-scrollbox"));
+    mWins.push(document.getElementById("profile-3d"));
+    mWins.push(document.getElementById("profile-third"));
+
+    for(const entry of mWins) {
+        mWinMov.push(changeWindowFunc(entry));
+    }
+};
+
+const changeWindowFunc = (targ) => () => {
+    for(const entry of mWins) {
+        entry.style.display = "none";
+    }
+    targ.style.display = "block";
+};
+
+var state = {mWinN:0};
+const mWinForw = () => {
+    if(state.mWinN >= mWins.length - 1) {
+        state.mWinN = 0;
+    } else {
+        state.mWinN += 1;
+    }
+    mWinMov[state.mWinN]();
+};
+const mWinBack = () => {
+    if(state.mWinN == 0) {
+        state.mWinN = mWins.length - 1;
+    } else {
+        state.mWinN -= 1;
+    }
+    mWinMov[state.mWinN]();
+};
+
+function closeButtonEvent() {
+    mWinForw();
+};
